1a1b4b41a21aebf3c7e668281a572c20076b854b
codlab <codlabtech@gmail.com>
2020-02-16 18:26:05 +0100

    prepare server

---
+++ front/build/precache-manifest.e23cf62b1db08ce418b4b39f5889d62d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a630945da97159009073652e079c73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e90667181f26380c4c84",
+    "url": "/static/css/main.11bba1e5.chunk.css"
+  },
+  {
+    "revision": "21273502b8a130bdf8f9",
+    "url": "/static/js/2.66cde5c9.chunk.js"
+  },
+  {
+    "revision": "e90667181f26380c4c84",
+    "url": "/static/js/main.f140d6a4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
