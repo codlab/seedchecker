350e44f35f9826b8d60b8a6c5fa53b5151fbda29
codlab <codlabtech@gmail.com>
2020-03-21 06:23:47 +0100

    add compilation work

--- app/server/arduino.ts
+++ app/server/arduino.ts
@@ -2,6 +2,25 @@ import { Router } from "express";
 import { v4 as uuidv4 } from 'uuid';
 import mkdir from "mkdir-promise";
 import copy from "recursive-copy";
+const { spawn } = require('child_process');
+
+interface Result {
+    output: string;
+    code: number;
+}
+
+function exec(bin: string, args: string[]): Promise<Result> {
+    return new Promise((resolve, reject) => {
+        var output = "";
+        const cmd = spawn(bin, args || []);
+        cmd.stdout.on("data", (data: any) => output += data);
+        cmd.stderr.on("data", (data: any) => output += data);
+
+        cmd.on('close', (code: any) => {
+            resolve({output, code});
+        });
+    });
+}
 
 export default class Arduino {
     public router: Router;
@@ -11,11 +30,13 @@ export default class Arduino {
 
         this.router.post("/compile", (req, res) => {
             const uuid = uuidv4();
-            const folder = `tmp/${uuid}/`;
+            const folder = `${__dirname}/tmp/${uuid}/`;
 
             mkdir(folder)
             .then(() => copy("/usr/local/arduino/src", folder))
-            .then(() => {
+            .then(() => exec("/bin/bash", ["-c", `cd ${folder}/ ; make all`] ))
+            .then((result: Result) => {
+                console.log("result", {result});
                 res.json({finished: uuid});
             })
             .catch((err: Error) => {
